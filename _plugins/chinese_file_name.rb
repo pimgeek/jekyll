e05fa6f29c52e457a5ab14f477416ae061c14529
ben7th <ben7th@sina.com>
2014-11-12 21:38:01 +0800

    a test

--- _plugins/chinese_file_name.rb
+++ _plugins/chinese_file_name.rb
@@ -1,18 +1,18 @@
-module Jekyll
-  class URL
-    def self.escape_path(path)
-      # Because URI.escape doesn't escape '?', '[' and ']' by default,
-      # specify unsafe string (except unreserved, sub-delims, ":", "@" and "/").
-      #
-      # URI path segment is defined in RFC 3986 as follows:
-      #   segment       = *pchar
-      #   pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
-      #   unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
-      #   pct-encoded   = "%" HEXDIG HEXDIG
-      #   sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
-      #                 / "*" / "+" / "," / ";" / "="
-      # URI.escape(path, /[^a-zA-Z\d\-._~!$&\'()*+,;=:@\/]/).encode('utf-8')
-      path
-    end
-  end
-end
+# module Jekyll
+#   class URL
+#     def self.escape_path(path)
+#       # Because URI.escape doesn't escape '?', '[' and ']' by default,
+#       # specify unsafe string (except unreserved, sub-delims, ":", "@" and "/").
+#       #
+#       # URI path segment is defined in RFC 3986 as follows:
+#       #   segment       = *pchar
+#       #   pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
+#       #   unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+#       #   pct-encoded   = "%" HEXDIG HEXDIG
+#       #   sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+#       #                 / "*" / "+" / "," / ";" / "="
+#       # URI.escape(path, /[^a-zA-Z\d\-._~!$&\'()*+,;=:@\/]/).encode('utf-8')
+#       path
+#     end
+#   end
+# end
