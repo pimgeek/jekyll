ee912ba7fbd4c39cde7f60d63cd1bcfc5cff3fbb
Kaid <kaid@kaid.me>
2014-11-13 17:33:30 +0800

    fixed indentation

--- sublime/NewWikiItemCommand.py
+++ sublime/NewWikiItemCommand.py
@@ -5,27 +5,28 @@ from os import path
 
 class NewWikiItemCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-  	active_window().show_input_panel("添加新的wiki条目", "", self.new_item, None, None)
+    active_window().show_input_panel("添加新的wiki条目", "", self.new_item, None, None)
 
-  def generate_permalink(self):
-  	return "%i.html" % (round(time.time()))
+def generate_permalink(self):
+  return "%i.html" % (round(time.time()))
 
-  def item_header(self, item_name):
-  	return ("---\n"
-  		    "layout: post\n"
-  		    "title: %s\n"
-  		    "permalink: %s\n"
-  		    "---\n"
-  		   ) % (item_name, self.generate_permalink())
+def item_header(self, item_name):
+  return ("---\n"
+          "layout: post\n"
+          "title: %s\n"
+          "permalink: %s\n"
+          "---\n"
+         ) % (item_name, self.generate_permalink())
 
-  def new_item(self, item_name):
-  	file_name = path.join(sublime.active_window().folders()[0], "_miniwiki", "%s.markdown" % item_name)
-  	if os.path.isfile(file_name):
-  		sublime.error_message("文件已存在！")
-  	else:
-  		file = open(file_name, "w")
+def new_item(self, item_name):
+  file_name = path.join(sublime.active_window().folders()[0], "_miniwiki", "%s.markdown" % item_name)
 
-  		file.write(self.item_header(item_name))
-  		file.close()
-
-  		active_window().open_file(file_name)
+  if os.path.isfile(file_name):
+    sublime.error_message("文件已存在！")
+  else:
+    file = open(file_name, "w")
+  
+    file.write(self.item_header(item_name))
+    file.close()
+    
+    active_window().open_file(file_name)
